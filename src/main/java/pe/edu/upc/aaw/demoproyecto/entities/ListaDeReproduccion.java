a825f455159f08cbd734cb678424aa74886f7e6f
Alvaro1503 <riquez.lazo@gmail.com>
2023-09-22 23:16:15 -0500

    .

--- src/main/java/pe/edu/upc/aaw/demoproyecto/entities/ListaDeReproduccion.java
+++ src/main/java/pe/edu/upc/aaw/demoproyecto/entities/ListaDeReproduccion.java
@@ -9,12 +9,6 @@ public class ListaDeReproduccion {
     private int idLista_de_Reproduccion;
     @Column(name = "nameLista_de_Reproduccion",nullable = false,length = 45)
     private String nameLista_de_Reproduccion;
-    @Column(name = "descriptionLista_de_Reproduccion",nullable = false,length = 100)
-    private String descriptionLista_de_Reproduccion;
-    @Column(name = "list_contentsLista_de_Reprodccion",nullable = false,length = 1000)
-    private String list_contentsLista_de_Reproduccion;
-    @Column(name = "type_list_CategoryLista_de_Reproduccion",nullable = false,length = 1000)
-    private String type_list_CategoryLista_de_Reproduccion;
     @ManyToOne
     @JoinColumn(name = "idUsuario")
     private Usuario usuario;
@@ -22,12 +16,10 @@ public class ListaDeReproduccion {
     public ListaDeReproduccion() {
     }
 
-    public ListaDeReproduccion(int idLista_de_Reproduccion, String nameLista_de_Reproduccion, String descriptionLista_de_Reproduccion, String list_contentsLista_de_Reproduccion, String type_list_CategoryLista_de_Reproduccion, Usuario usuario) {
+    public ListaDeReproduccion(int idLista_de_Reproduccion, String nameLista_de_Reproduccion, Usuario usuario) {
         this.idLista_de_Reproduccion = idLista_de_Reproduccion;
         this.nameLista_de_Reproduccion = nameLista_de_Reproduccion;
-        this.descriptionLista_de_Reproduccion = descriptionLista_de_Reproduccion;
-        this.list_contentsLista_de_Reproduccion = list_contentsLista_de_Reproduccion;
-        this.type_list_CategoryLista_de_Reproduccion = type_list_CategoryLista_de_Reproduccion;
+
         this.usuario = usuario;
     }
 
@@ -45,34 +37,6 @@ public class ListaDeReproduccion {
 
     public void setNameLista_de_Reproduccion(String nameLista_de_Reproduccion) {
         this.nameLista_de_Reproduccion = nameLista_de_Reproduccion;
-    }
-
-    public String getDescriptionLista_de_Reproduccion() {
-        return descriptionLista_de_Reproduccion;
-    }
-
-    public void setDescriptionLista_de_Reproduccion(String descriptionLista_de_Reproduccion) {
-        this.descriptionLista_de_Reproduccion = descriptionLista_de_Reproduccion;
-    }
-
-    public String getList_contentsLista_de_Reprodccion() {
-        return list_contentsLista_de_Reproduccion;
-    }
-
-    public void setList_contentsLista_de_Reproduccion(String list_contentsLista_de_Reproduccion) {
-        this.list_contentsLista_de_Reproduccion = list_contentsLista_de_Reproduccion;
-    }
-
-    public String getType_list_CategoryLista_de_Reproduccion() {
-        return type_list_CategoryLista_de_Reproduccion;
-    }
-
-    public void setType_list_CategoryLista_de_Reproduccion(String type_list_CategoryLista_de_Reproduccion) {
-        this.type_list_CategoryLista_de_Reproduccion = type_list_CategoryLista_de_Reproduccion;
-    }
-
-    public String getList_contentsLista_de_Reproduccion() {
-        return list_contentsLista_de_Reproduccion;
     }
 
     public Usuario getUsuario() {
