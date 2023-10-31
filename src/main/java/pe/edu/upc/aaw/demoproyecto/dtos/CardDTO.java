15df4445d470f2d41cc772c7bcd670a506aaadb1
DiegoAlban1223 <82966155+DiegoAlban1223@users.noreply.github.com>
2023-10-31 11:06:59 -0500

    actualizacion tabla card

--- src/main/java/pe/edu/upc/aaw/demoproyecto/dtos/CardDTO.java
+++ src/main/java/pe/edu/upc/aaw/demoproyecto/dtos/CardDTO.java
@@ -2,9 +2,14 @@ package pe.edu.upc.aaw.demoproyecto.dtos;
 
 import pe.edu.upc.aaw.demoproyecto.entities.Pago;
 
+import java.util.Date;
+
 public class CardDTO {
     private int idCard;
     private String typeTarjeta;
+    private String nameTarjeta;
+    private Date FechaVencimiento;
+    private int CVV;
 
 
     public int getIdCard() {
@@ -23,5 +28,27 @@ public class CardDTO {
         this.typeTarjeta = typeTarjeta;
     }
 
+    public String getNameTarjeta() {
+        return nameTarjeta;
+    }
 
+    public void setNameTarjeta(String nameTarjeta) {
+        this.nameTarjeta = nameTarjeta;
+    }
+
+    public Date getFechaVencimiento() {
+        return FechaVencimiento;
+    }
+
+    public void setFechaVencimiento(Date fechaVencimiento) {
+        FechaVencimiento = fechaVencimiento;
+    }
+
+    public int getCVV() {
+        return CVV;
+    }
+
+    public void setCVV(int CVV) {
+        this.CVV = CVV;
+    }
 }
--- src/main/java/pe/edu/upc/aaw/demoproyecto/entities/Card.java
+++ src/main/java/pe/edu/upc/aaw/demoproyecto/entities/Card.java
@@ -3,6 +3,7 @@ package pe.edu.upc.aaw.demoproyecto.entities;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import javax.persistence.*;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -13,16 +14,23 @@ public class Card {
     private int idCard;
     @Column(name = "typeTarjeta", nullable = false)
     private String typeTarjeta;
-
+    @Column(name = "nameTareta", nullable = false)
+    private String nameTarjeta;
+    @Column(name = "FechaVencimiento", nullable = false)
+    private Date FechaVencimiento;
+    @Column(name = "CVV", nullable = false)
+    private int CVV;
 
 
     public Card() {
     }
 
-    public Card(int idCard, String typeTarjeta) {
+    public Card(int idCard, String typeTarjeta, String nameTarjeta, Date fechaVencimiento, int CVV) {
         this.idCard = idCard;
         this.typeTarjeta = typeTarjeta;
-
+        this.nameTarjeta = nameTarjeta;
+        FechaVencimiento = fechaVencimiento;
+        this.CVV = CVV;
     }
 
     public int getIdCard() {
@@ -41,5 +49,27 @@ public class Card {
         this.typeTarjeta = typeTarjeta;
     }
 
+    public String getNameTarjeta() {
+        return nameTarjeta;
+    }
 
+    public void setNameTarjeta(String nameTarjeta) {
+        this.nameTarjeta = nameTarjeta;
+    }
+
+    public Date getFechaVencimiento() {
+        return FechaVencimiento;
+    }
+
+    public void setFechaVencimiento(Date fechaVencimiento) {
+        FechaVencimiento = fechaVencimiento;
+    }
+
+    public int getCVV() {
+        return CVV;
+    }
+
+    public void setCVV(int CVV) {
+        this.CVV = CVV;
+    }
 }
